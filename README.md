751c8c990179c845a880c2bd42bdaff7bbb92023
maohongbin01 <maohongbin01@baidu.com>
2017-02-15 17:26:18 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ApkHack
+
+## What is ApkHack?
+
+ApkHack can turn Android APK into java source code and resource files including AndroidManifest.xml and layout.
+
+### How to use
+./apkHack.sh <path-to-file/xxx.apk>
+The results will be in the output dir like below:
+
+output
+  ->AndroidManifest.xml
+  ->assets
+  ->lib
+  ->res
+    ->layout
+      ->activity_main.xml
+    ->values
+    ->....
+  ->classes.dex-src
+    ->com.xxx.xxx.MainActivity.java
+  ->classes2.dex-src
+  ->.....(multi-dex) 
---
+++ apkHack.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+usage() {
+    echo ""
+    echo "需要命令参数："
+    echo "./apkHack <xxx.apk>"
+    echo ""
+    exit 255
+}
+
+APK=$1
+if [ -z "$APK" ] || [ "${APK:0-4}" != ".apk" ] || [ ! -f "$APK" ]; then
+  usage
+fi
+echo "Using apkHack on $1"
+if [ -d "output" ]; then
+  rm -r output
+fi
+mkdir -p output/tmp
+tools/Apktool/apktool d -f -s -o output/tmp $APK
+mv output/tmp/AndroidManifest.xml output/
+mv output/tmp/assets output/
+mv output/tmp/lib output/
+mv output/tmp/res output/
+for file in `ls output/tmp |grep ".dex"`
+do
+  mkdir -p output/$file-src
+  tools/dex2jar-2.0/d2j-dex2jar.sh -f -n -os output/tmp/$file -o output/tmp/$file.jar
+  java -jar tools/procyon-decompiler-0.5.30.jar -jar output/tmp/$file.jar -o output/$file-src/
+done
+rm -r output/tmp
